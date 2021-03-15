4b3573e2c5c9047d33da0262e1c85daeddc02c3d
18a10010221@students.hou.edu.vn <18a10010221@students.hou.edu.vn>
2021-03-15 19:23:17 +0700

    ahsfdahfasdh

--- QuanlyCongvan.BRL/LichsucongtacBRL.cs
+++ QuanlyCongvan.BRL/LichsucongtacBRL.cs
@@ -20,5 +20,11 @@ namespace QuanlyCongvan.BRL
             gltsLichsuCongtacCanbo = gltsLichsucongtac(0).FindAll(lichsu => lichsu.FK_iCanboID == iLichsuCongtacCanbo);
             return gltsLichsuCongtacCanbo;
         }
+        public List<LichsucongtacEntity> gltsTest(long iLichsuCongtacCanbo)
+        {
+            List<LichsucongtacEntity> gltsLichsuCongtacCanbo = new List<LichsucongtacEntity>();
+            gltsLichsuCongtacCanbo = gltsLichsucongtac(0).FindAll(lichsu => lichsu.FK_iCanboID == iLichsuCongtacCanbo);
+            return gltsLichsuCongtacCanbo;
+        }
     }
 }
